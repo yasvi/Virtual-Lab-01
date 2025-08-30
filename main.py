f45f3f109e09f0ba8b4a31ef83f7014744dfc66a
SV7OFFICIAL <sankam.yasovardan7b@gmail.com~>
2025-08-30 14:45:49 +0530

    V3

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#=====================
+# IMPORT STATEMENTS
+#=====================
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
@@ -5,7 +8,17 @@ import mysql.connector as cc
 from mysql.connector import IntegrityError
 from random import randint
 
+#==========================================
+
+#   BEFORE RUNNING READ THE README FILE
+
+#==========================================
+
 app = Ursina()
+
+#==============================
+# SQL DATABASE CONNECTION
+#==============================
 
 db={
     'host':'localhost',
@@ -26,14 +39,17 @@ conn.commit()
 
 
 
-# player settings
+# PLAYER SETTINGS
 player =FirstPersonController()
-player.position = (0,-26,-10)
-
+player.position = (0,-23,21.5)
 player.gravity=0.5
 player.camera_pivot.y=5
 
 # EditorCamera()
+
+#===================
+# CREDENTIAL ROOM
+#===================
 
 def cred_room():
     
@@ -90,17 +106,19 @@ def cred_room():
     signin ,login ,signup_panel,\
     login_panel,user_s,passwords_s,\
     msg_s,user_l,passwords_l,continue_s\
-    ,continue_l,msg_l
+    ,continue_l,msg_l,cancel_s,cancel_l\
+    ,bg,text_l,text_s,click,cred_bg
 
-    login_door=Entity(model='cube',
-                      scale=7,
-                      position=(-16,-26,-12),
-                      color=color.red,
+    login_door=Entity(model='assets/rift.glb',
+                      scale=4,
+                      position=(-16,-24,-12),
+                      color=color.rgb(255,0,0),
+                      rotation_z=180,
                       collider='box')
     
-    signup_door=Entity(model='cube',
-                       scale=7,
-                       position=(16,-26,-12),
+    signup_door=Entity(model='assets/rift.glb',
+                       scale=4,
+                       position=(16,-24,-12),
                        collider='box',
                        color=color.blue)
 
@@ -125,49 +143,114 @@ def cred_room():
                 parent=login_door,
                 billboard=True,
                 z=-0.1,
+    
                 )
     
+    # SOUNDS
+    click= Audio('assets/click.wav',autoplay=False)
+    cred_bg=Audio('assets/abg.mp3',autoplay=False,loop=True)
+    cred_bg.volume=5
+    cred_bg.play()
 
-    signup_panel=Entity(enabled=False,parent=camera.ui)
+    # LOGIN ENTITY
     login_panel=Entity(enabled=False,parent=camera.ui)
-    user_l = InputField(y=0,parent=login_panel)
-    passwords_l = InputField(y=-0.2,parent=login_panel,password=True)
-    
-    user_s = InputField(y=0,parent=signup_panel)
-    passwords_s = InputField(y=-0.2,parent=signup_panel,password=True)
-    msg_s = Text("", parent=signup_panel, y=0.3, color=color.black)
-    msg_l = Text("", parent=login_panel, y=0.3, color=color.black)
-    continue_s=Button('continue',parent=signup_panel,y=-0.3)
-    continue_l=Button('continue',parent=login_panel,y=-0.3)
+
+    user_l = InputField('Username',y=0,parent=login_panel,
+                        color=color.rgba(0.5,0.5,0.5,0.6),z=-1)
+    user_l.highlight_color=color.rgba(0.5,0.5,0.5,0.6)
+
+    passwords_l = InputField('Password',y=-0.2,parent=login_panel,password=True,
+                             color=color.rgba(0.5,0.5,0.5,0.6),z=-1)
+    passwords_l.highlight_color=color.rgba(0.5,0.5,0.5,0.6)
+    passwords_l.hide_content=True
+   
+    msg_l = Text("", parent=login_panel, y=-0.1,x=-0.08, color=color.black)
+
+    continue_l=Button('Continue',parent=login_panel,y=-0.3,x=-0.1,scale=(0.15,0.1),
+                      color=color.rgba(0.5,0.5,0.5,0.6))
+    continue_l.text_entity.scale=8
+
+    cancel_l=Button('Cancel',parent=login_panel,y=-0.3,x=0.1,scale=0.1,
+                    color=color.rgba(0.5,0.5,0.5,0.6))
+
+    text_l=Text('Login',parent=login_panel,y=0.3,x=-0.16,scale=5,color=color.white,z=-1)
+
+
+    #SIGNUP ENTITY
+    signup_panel=Entity(enabled=False,parent=camera.ui) 
+
+    user_s = InputField('Username',y=0,parent=signup_panel,z=-1,
+                        color=color.rgba(0.5,0.5,0.5,0.6))
+    user_s.highlight_color=color.rgba(0.5,0.5,0.5,0.6)
+
+    passwords_s = InputField('Password',y=-0.2,parent=signup_panel,password=True,z=-1,
+                             color=color.rgba(0.5,0.5,0.5,0.6))
+    passwords_s.highlight_color=color.rgba(0.5,0.5,0.5,0.6)
+    passwords_s.hide_content=True
+
+    msg_s = Text("", parent=signup_panel, y=-0.1,x=-0.08, color=color.black)
+
+    continue_s=Button('Continue',parent=signup_panel,y=-0.3,z=-1,x=-0.1,scale=(0.15,0.1),
+                      color=color.rgba(0.5,0.5,0.5,0.6))
+    continue_s.text_entity.scale=8
+
+    cancel_s=Button('Cancel',parent=signup_panel,y=-0.3,x=0.1,scale=0.1,z=-1,
+                    color=color.rgba(0.5,0.5,0.5,0.6))
+
+    text_s=Text('Sign up',parent=signup_panel,y=0.3,x=-0.2,scale=5,
+                color=color.white,z=-1)
+
+    bg= Button(parent=camera.ui,
+               color=color.white,
+               scale=(2.5,3),
+               position=(0,0),
+               enabled=False,z=1,
+               texture='assets/auth.jpg',
+               texture_scale=(1.5,1.5))
 
     cred_entities.extend([login_door,signup_door,light,signin,login])
     
-    
-
 cred_room()
+
+#=================================
+# FUNCTIONS OF BUTTONS AND PANELS
+#=================================
 
 def show_sign_panel():
     player.enabled=False
     mouse.locked=False
     signup_panel.enabled=True
+    bg.enabled=True
 
 def hide_sign_panel():
     player.enabled=True
     mouse.locked=True
     signup_panel.enabled=False
+    bg.enabled=False
+
+def can_l():
+    click.play()
+    hide_login_panel()
+
+def can_s():
+    click.play()
+    hide_sign_panel()
 
 def show_login_panel():
     player.enabled=False
     mouse.locked=False
     login_panel.enabled=True
-
+    bg.enabled=True
+    
+    
 def hide_login_panel():
     player.enabled=True
     mouse.locked=True
     login_panel.enabled=False
+    bg.enabled=False
 
 def continue_login():
-    if user_l.text and passwords_l.text:
+    if user_l.text and passwords_l.text and user_l.text !='Username' and passwords_l.text!='Password':
         username = user_l.text.strip()
         password = passwords_l.text.strip()
         cursor=conn.cursor()
@@ -183,8 +266,12 @@ def continue_login():
             return
         
         if result:
+                  click.play()
+                  cred_bg.stop()
                   hide_login_panel()
-                  player.position=Vec3(0,0,16) 
+                  player.position=Vec3(0,0,16)
+                  voice.play() 
+                  siren.play()
         else:
              msg_l.text="login error"
              msg_l.color=color.white                  
@@ -195,7 +282,7 @@ def continue_login():
   
 
 def continue_sign():
-    if user_s.text and passwords_s.text:
+    if user_s.text and passwords_s.text and user_s.text !='Username' and passwords_s.text!='Password':
         username = user_s.text.strip()
         password = passwords_s.text.strip()
         cursor=conn.cursor()
@@ -209,7 +296,11 @@ def continue_sign():
             return
             
         conn.commit()
+        click.play()
+        cred_bg.stop()
         hide_sign_panel()
+        voice.play()
+        siren.play()
         player.position=Vec3(0,0,16)
     
     else:
@@ -217,35 +308,50 @@ def continue_sign():
         
         
 continue_s.on_click=continue_sign
-continue_l.on_click=continue_login 
+continue_l.on_click=continue_login
+cancel_l.on_click=can_l
+cancel_s.on_click=can_s
+    
+#================
+# GAME MECHANICS
+#================
 
-
-
-#game mechanics
 def update():
     if held_keys['b']:
-        Dlight.color=color.rgba(0.9,0,0,80)
+        Dlight.color=color.rgba(0.5,0,0,80)
        
     if held_keys['escape']:
         application.quit()
 
-    if distance(player.position,signup_door.position)<15 and held_keys['e']:
+    if distance(player.position,signup_door.position)<7 and held_keys['e']:
+        click.play()
         show_sign_panel()
 
     else:
         signin.enabled=False
 
-    if distance(player.position,login_door.position)<15 and held_keys['e']:
+    if distance(player.position,login_door.position)<7 and held_keys['e']:
+        click.play()
         show_login_panel()
+        
 
     else:
         login.enabled=False
 
+    bg.rotation_z += 1 * time.dt 
 
-#lighting
+def power():
+    Dlight.color=color.rgba(0.5,0.7,0.5,180)
+    pow.color=color.green
+    voice.stop()
+    siren.stop()
+    charge_m.play()
+    bg_m()
+    
+#LIGHTING
 Dlight=DirectionalLight()
 Dlight.look_at(Vec3(1,-1,-1))
-Dlight.color=color.rgba(0.3,0,0,80)
+Dlight.color=color.rgba(0.5,0,0,80)
 Dlight.parent=scene
 
 Alight=AmbientLight()
@@ -258,8 +364,23 @@ flicker_light.position = Vec3(20, 5, 20)
 flicker_light.parent = scene
 
 
+#SOUND
+voice=Audio('assets/main_v.mp3',autoplay=False,loop=True)
+siren=Audio('assets/main_a.mp3',autoplay=False,loop=True)
+charge_m=Audio('assets/charge.mp3',autoplay=False)
+main_bg=Audio('assets/mbg.mp3',autoplay=False,loop=True)
+main_bg.volume=6
+voice.volume=8
+siren.volume=0.6
 
-#add floor
+def bg_m():
+    main_bg.play()
+    
+#========================
+# ENTITIES FOR MAIN ROOM
+#========================
+
+#ADD FLOOR
 Entity(model='plane',
        scale=50,
        collider='box',
@@ -270,7 +391,7 @@ Entity(model='plane',
 )
 
 
-#add ceiling
+#ADD CEILNG
 Entity(model='cube',
        color=color.white,
        scale=(1,50,50),
@@ -282,8 +403,8 @@ Entity(model='cube',
        shader=lit_with_shadows_shader
 )
 
-#add walls
-Entity(model='cube',
+#ADD WALLS
+wall5=Entity(model='cube',
        scale=(1,20,50),
        position=(-25,2.5,0),
        texture='assets/wall.jpeg',
@@ -320,7 +441,20 @@ Entity(model='cube',
        color=color.white,
        shader=lit_with_shadows_shader)
 
-#add tables
+# ADD POWER
+pow_entity=Entity(position=(-24,8,8),
+           model='cube',
+           scale=1,
+           color=color.black)
+
+pow=Button(position=(0,0,1),
+           model='cube',
+           parent=pow_entity,
+           scale=1,
+           color=color.red,
+           on_click=power)
+
+#ADD TABLES
 Entity(model='assets/table.glb',
        scale=5,
        position=(-16,1.8,-12),
@@ -362,7 +496,7 @@ Entity(model='assets/table.glb',
        color=color.white,
        shader=lit_with_shadows_shader)
 
-#add Bunsen burner
+#ADD BUNSEN BUNER
 Entity(model='assets/bunsen_burner.glb',
        scale=7.5,
        position=(16,3.8,12),
@@ -405,7 +539,7 @@ Entity(model='assets/bunsen_burner.glb',
        color=color.white,
        shader=lit_with_shadows_shader)
 
-#Add testube stand
+#ADD TESTUBE STAND
 Entity(model='assets/testubes.glb',
        scale=1.3,
        position=(16,3.8,15),
@@ -455,7 +589,7 @@ Entity(model='assets/testubes.glb',
        rotation_y=270)
     
 
-# Add hologram
+# ADD HOLOGRAM
 Entity(model= 'assets/hologram.glb',
        position=(0,4,0),
        scale=2.5,
@@ -464,7 +598,7 @@ Entity(model= 'assets/hologram.glb',
        shader=lit_with_shadows_shader)
 
 
-#Add shelf
+#ADD SHELF
 Entity(model='book_shelf.glb',
        scale =0.04,
        position=(-24,0,0),
@@ -473,58 +607,136 @@ Entity(model='book_shelf.glb',
        shader=lit_with_shadows_shader,
        rotation_y=270)
 
-#add flame
+#ADD FLAME
 Entity(model='cube',
        scale=(0.05,0.05),
        collider='box',
        color=color.blue,
        position=(0,5,12),
-       rotation_x=90)
+       rotation_x=90,
+       shader=lit_with_shadows_shader)
 
 Entity(model='cube',
        scale=(0.05,0.05),
        collider='box',
        color=color.blue,
        position=(0,5,-12),
-       rotation_x=90)
+       rotation_x=90,
+       shader=lit_with_shadows_shader)
 
 Entity(model='cube',
        scale=(0.05,0.05),
        collider='box',
        color=color.blue,
        position=(16,5,12),
-       rotation_x=90)
+       rotation_x=90,
+       shader=lit_with_shadows_shader)
 
 Entity(model='cube',
        scale=(0.05,0.05),
        collider='box',
        color=color.blue,
        position=(-16,5,12),
-       rotation_x=90)
+       rotation_x=90,
+       shader=lit_with_shadows_shader)
 
 Entity(model='cube',
        scale=(0.05,0.05),
        collider='box',
        color=color.blue,
        position=(16,5,-12),
-       rotation_x=90)
+       rotation_x=90,
+       shader=lit_with_shadows_shader)
 
 Entity(model='cube',
        scale=(0.05,0.05),
        collider='box',
        color=color.blue,
        position=(-16,5,-12),
-       rotation_x=90)
+       rotation_x=90,
+       shader=lit_with_shadows_shader)
 
-# Add chemicals
-Mg=Entity(model='cube',
-       scale=0.5,
+# ADD CHEMICALS
+Mg=Entity(model='assets/bottles.glb',
+       scale=0.2,
        collider='box',
        color=color.white,
-       position=(-23,4.3,0)
-       )
+       position=(-23,4.3,2),
+       shader=lit_with_shadows_shader)
 
+Na=Entity(model='assets/bottles.glb',
+       scale=0.2,
+       collider='box',
+       color=color.white,
+       position=(-23,7,2),
+       shader=lit_with_shadows_shader)
+
+Al=Entity(model='assets/bottles.glb',
+       scale=0.2,
+       collider='box',
+       color=color.white,
+       position=(-23,5.5,2),
+       shader=lit_with_shadows_shader)
+
+Ca=Entity(model='assets/bottles.glb',
+       scale=0.2,
+       collider='box',
+       color=color.white,
+       position=(-23,3,2),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/canister.glb',
+       scale=0.006,
+       collider='box',
+       color=color.white,
+       position=(-16,3.8,10),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/canister.glb',
+       scale=0.006,
+       collider='box',
+       color=color.white,
+       position=(16,3.8,10),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/canister.glb',
+       scale=0.006,
+       collider='box',
+       color=color.white,
+       position=(-16,3.8,-15),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/canister.glb',
+       scale=0.006,
+       collider='box',
+       color=color.white,
+       position=(16,3.8,-15),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/o2.glb',
+       scale=1.5,
+       collider='box',
+       color=color.white,
+       position=(16,7,-23.8),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/o2.glb',
+       scale=1.5,
+       collider='box',
+       color=color.white,
+       position=(17,7,-23.8),
+       shader=lit_with_shadows_shader)
+
+Entity(model='assets/gas.glb',
+       scale=5,
+       collider='box',
+       color=color.white,
+       position=(18,7,-23.8),
+       rotation_y=180,
+       shader=lit_with_shadows_shader)
 app.run()
+
+#=========================================#
 
 
 
